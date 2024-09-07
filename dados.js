9586fc13cc919e4cf7c3968e7c604fa2efb397f4
RuivoProgramador <126624700+RuivoProgramador@users.noreply.github.com>
2024-09-07 19:33:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,35 @@
+/*LET = Essa palavra-chave é usada para declarar uma variável em JavaScript.*/ 
+/*Dados = Esse é o nome que você escolheu para a variável. Você poderia usar qualquer nome válido.*/ 
+
+let dados = [
+  {
+    titulo : "Rodrigo Garro",
+    descricao : "Rodrigo Garro é um meio-campista argentino que conquistou o coração da Fiel Torcida. Conhecido por sua habilidade técnica e visão de jogo, o meia tem se destacado com a camisa do Corinthians.",
+    link : "https://pt.wikipedia.org/wiki/Rodrigo_Garro",
+    tags: "Meio Campo",
+    },
+{ 
+    titulo : "Hugo Souza",
+    descricao : "Hugo Souza é um goleiro brasileiro que se destacou nas categorias de base do Flamengo e agora defende as cores do Corinthians.",
+    link : "https://pt.wikipedia.org/wiki/Hugo_Souza",
+    tags: "Goleiro",
+},
+    {
+titulo : "Fagner",
+descricao : "Fagner é um lateral-direito brasileiro, conhecido por sua experiência e liderança em campo. É um dos principais jogadores do Corinthians.",
+link : "https://pt.wikipedia.org/wiki/Fagner_(futebolista)",
+tags: "Lateral"
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
